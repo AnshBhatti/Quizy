3a98061d07c6fa65911d68d9eaeb25e3a50f665f
AnshBhatti <44926412+AnshBhatti@users.noreply.github.com>
2021-08-26 12:33:35 -0400

    Update settings.py

--- Quizy/settings.py
+++ Quizy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w!7w7xj8dz8$7g@y@zb=7mac$sig0tiva-sm)x2u#46su(2!30'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
